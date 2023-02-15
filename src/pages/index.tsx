c00d3583c9d9632a3aa04d19372dcd9bf16f18db
Trent Deckard <tdeckard2000@yahoo.com>
2023-02-15 11:34:57 -0500

    added mah face

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,17 +19,19 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <div className={styles.headerContainer}>
-          <img
-            className={styles.headshot}
-            src="/headshot_placeholder.jpg"
-            alt=""
-          />
+          <div className={styles.headshotContainer}>
+            <img className={styles.headshotFaded} src="/me_arms_out_faded.jpg" alt="" />
+            <img className={styles.headshot} src="/me_arms_out.jpg" alt="" />
+          </div>
           <div className={styles.titleContainer}>
             <h4 className={styles.titleTop}>Trent</h4>
             <h4 className={styles.titleBottom}>Endeavors</h4>
+            <div>
             <p className={styles.titleParagraph}>
-              A not-so comprehensive list of my Web Dev, Electronic, 3D Modeling, iOS Native, and Video projects.
+              A not-so comprehensive list of my Web Dev, Electronic, 3D
+              Modeling, iOS Native, and Video projects.
             </p>
+            </div>
           </div>
         </div>
       </main>
