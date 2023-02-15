33c1c9ed6d30c8bfa2020d71db2e59ea45840194
Trent Deckard <tdeckard2000@yahoo.com>
2023-02-14 19:02:34 -0500

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,38 @@
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "@/styles/Home.module.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Trent Endeavors</title>
+        <meta
+          name="description"
+          content="All about Trent's attempts to make interesting things."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.main}>
+        <div className={styles.headerContainer}>
+          <img
+            className={styles.headshot}
+            src="/headshot_placeholder.jpg"
+            alt=""
+          />
+          <div className={styles.titleContainer}>
+            <h4 className={styles.titleTop}>Trent</h4>
+            <h4 className={styles.titleBottom}>Endeavors</h4>
+            <p className={styles.titleParagraph}>
+              A not-so comprehensive list of my Web Dev, Electronic, 3D Modeling, iOS Native, and Video projects.
+            </p>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
