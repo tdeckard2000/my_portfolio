0864337b4e9f75723730ee865962cbbc0db79958
Trent Deckard <tdeckard2000@yahoo.com>
2023-02-16 16:43:21 -0500

    small changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,13 +6,15 @@ import { useEffect, useState } from "react";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const [mobileHeaderOffset, setMobileHeaderOffset] = useState("0");
-  const [desktopHeaderOffset, setDesktopHeaderOffset] = useState("0");
+  // const [mobileHeaderOffset, setMobileHeaderOffset] = useState("0");
+  // const [desktopHeaderOffset, setDesktopHeaderOffset] = useState("0");
+  const [yPosition, setYPosition] = useState(0);
   useEffect(() => {
     const scroll = () => {
       const { pageYOffset } = window;
-      setMobileHeaderOffset(pageYOffset / 3 + "px");
-      setDesktopHeaderOffset(pageYOffset / 2.6 + "px");
+      setYPosition(pageYOffset);
+      // setMobileHeaderOffset(pageYOffset / 3 + "px");
+      // setDesktopHeaderOffset(pageYOffset / 2.6 + "px");
     };
     document.addEventListener("scroll", scroll);
     () => document.removeEventListener("scroll", scroll);
@@ -33,13 +35,13 @@ export default function Home() {
         <div className={styles.headerContainer}>
           <div className={styles.headshotContainer}>
             <img
-              style={{ bottom: desktopHeaderOffset }}
+              style={{ bottom: yPosition / 2.6 + "px" }}
               className={styles.headshotFaded}
               src="/me_faded_side.jpg"
               alt=""
             />
             <img
-              style={{ top: mobileHeaderOffset }}
+              style={{ top: yPosition / 3 + "px" }}
               className={styles.headshot}
               src="/me_faded_bottom.jpg"
               alt=""
@@ -58,15 +60,30 @@ export default function Home() {
         </div>
         <div className={styles.body}>
           <div
-            className={styles.navContainerDesktop}>
-            <span>Contact</span>
+            className={styles.navContainerDesktop}
+            style={{ bottom: yPosition / 4 + "px" }}
+          >
             <span>Web</span>
             <span>Electronics</span>
             <span className={styles.buttonModeling}>Modeling</span>
             <span>Video</span>
-            <span>More</span>
+            <span>Random</span>
+            <span className={styles.buttonContact}>Contact</span>
           </div>
-          <div className={styles.contactSection}>Contact Me</div>
+          {/* <div className={styles.contactSection}>
+            <h1>Trent Deckard</h1>
+            <div>
+              <h3>Contact</h3>
+              <a href="tel:+8123605563">812.360.5563</a>
+              <a href="mailto: tredeckard@gmail.com">tredeckard@gmail.com</a>
+            </div>
+            <div>
+              <h3>Social Media</h3>
+              <a href="http://linkedin.com/in/trentdeckard">LinkedIn</a>
+              <a href="https://github.com/tdeckard2000">GitHub</a>
+              <a href="http://youtube.com/interestingted">YouTube</a>
+            </div>
+          </div> */}
         </div>
       </main>
     </>
