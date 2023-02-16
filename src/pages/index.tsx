444ec0b72741e2632b7a21acd8e92c1c9f258102
Trent Deckard <tdeckard2000@yahoo.com>
2023-02-16 03:08:38 -0500

    add parallax effect to mobile

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,21 @@
 import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
+import { useEffect, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [mobileHeaderOffset, setMobileHeaderOffset] = useState("0");
+  useEffect(() => {
+    const scroll = () => {
+      const { pageYOffset } = window;
+      setMobileHeaderOffset(pageYOffset / 3 + "px");
+    };
+    document.addEventListener("scroll", scroll);
+    () => document.removeEventListener("scroll", scroll);
+  }, []);
+
   return (
     <>
       <Head>
@@ -20,17 +30,26 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.headerContainer}>
           <div className={styles.headshotContainer}>
-            <img className={styles.headshotFaded} src="/me_arms_out_faded.jpg" alt="" />
-            <img className={styles.headshot} src="/me_faded_bottom.jpg" alt="" />
+            <img
+              className={styles.headshotFaded}
+              src="/me_arms_out_faded.jpg"
+              alt=""
+            />
+            <img
+              style={{ top: mobileHeaderOffset }}
+              className={styles.headshot}
+              src="/me_faded_bottom.jpg"
+              alt=""
+            />
           </div>
           <div className={styles.titleContainer}>
             <h4 className={styles.titleTop}>Trent</h4>
             <h4 className={styles.titleBottom}>Endeavors</h4>
             <div>
-            <p className={styles.titleParagraph}>
-              A not-so comprehensive list of my Web Dev, Electronic, 3D
-              Modeling, iOS Native, and Video projects.
-            </p>
+              <p className={styles.titleParagraph}>
+                A not-so comprehensive list of my Web Dev, Electronic, 3D
+                Modeling, iOS Native, and Video projects.
+              </p>
             </div>
           </div>
         </div>
