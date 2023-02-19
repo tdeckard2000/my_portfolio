20caa50e4b17991dc7430b942a4846eaab6f02f1
Trent Deckard <tredeckard@gmail.com>
2023-02-19 11:29:24 -0500

    add contact section & clean up code

---
+++ src/data/project_data.tsx
@@ -0,0 +1,170 @@
+export const webDevelopmentProjects = [
+    {
+        title: 'Meter Wiring Guide',
+        image: 'https://raw.githubusercontent.com/tdeckard2000/wiringGuide/main/images/Example.png',
+        imageAdjustments: { objectPosition: "-5px -48px", width: '532px' },
+        description: '',
+        pageLink: ''
+    },
+    {
+        title: 'Yolo Logan!',
+        image: 'https://raw.githubusercontent.com/tdeckard2000/YoloLogan/master/images/example.png',
+        imageAdjustments: { objectPosition: "-54px 10px", width: '379px' },
+        description: '',
+        pageLink: ''
+    },
+    {
+        title: 'Ted Counter',
+        image: 'https://raw.githubusercontent.com/tdeckard2000/TedCounter/master/IMG_0729.jpg',
+        imageAdjustments: { objectPosition: "-4px -44px", width: '356px' },
+        description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi 
+      ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit 
+      in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint 
+      occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+        pageLink: ''
+    },
+    {
+        title: 'Support Ticket Dashboard',
+        image: 'https://raw.githubusercontent.com/tdeckard2000/Support-Ticket-Dashboard/master/images/example.png',
+        imageAdjustments: { objectPosition: "-17px -3px", width: '537px' },
+        description: '',
+        pageLink: ''
+    },
+    {
+        title: 'Rate-A-Coworker 2.0',
+        image: 'https://media.istockphoto.com/id/1303510161/photo/abstract-empty-room.jpg?s=612x612&w=0&k=20&c=tfKPp0pwOyXNvuRZwWryADl9jdtEFltX2Cc-C2qFfY8=',
+        imageAdjustments: { objectPosition: "-27px 1px", width: '330px' },
+        description: '',
+        pageLink: ''
+    },
+    {
+        title: 'Fancy To-Do List',
+        image: 'https://media.istockphoto.com/id/1303510161/photo/abstract-empty-room.jpg?s=612x612&w=0&k=20&c=tfKPp0pwOyXNvuRZwWryADl9jdtEFltX2Cc-C2qFfY8=',
+        imageAdjustments: { objectPosition: "-27px 1px", width: '330px' },
+        description: '',
+        pageLink: ''
+    },
+    {
+        title: 'Simon Says Game',
+        image: 'https://media.istockphoto.com/id/1303510161/photo/abstract-empty-room.jpg?s=612x612&w=0&k=20&c=tfKPp0pwOyXNvuRZwWryADl9jdtEFltX2Cc-C2qFfY8=',
+        imageAdjustments: { objectPosition: "-27px 1px", width: '330px' },
+        description: '',
+        pageLink: ''
+    },
+    {
+        title: 'Portfolio Site',
+        image: 'https://media.istockphoto.com/id/1303510161/photo/abstract-empty-room.jpg?s=612x612&w=0&k=20&c=tfKPp0pwOyXNvuRZwWryADl9jdtEFltX2Cc-C2qFfY8=',
+        imageAdjustments: { objectPosition: "-27px 1px", width: '330px' },
+        description: "You're looking at it. Just a confirmation that I did indeed make this.",
+        pageLink: ''
+    }
+];
+
+export const electronicProjects = [
+    {
+        title: "Rex 'Battle Bot' R/C",
+        image: 'rex.GIF',
+        imageAdjustments: { objectPosition: "-52px -3px", width: '400px' },
+        description: '',
+        pageLink: ''
+    },
+    {
+        title: 'Knock to Unlock',
+        image: 'https://raw.githubusercontent.com/tdeckard2000/door_knock_unlock_arduino/main/images/IMG_7573.jpeg',
+        imageAdjustments: { objectPosition: "-33px 1px", width: '309px' },
+        description: '',
+        pageLink: ''
+    },
+    {
+        title: 'Cat Fan Deterrent',
+        image: 'https://raw.githubusercontent.com/tdeckard2000/cat_fan_deterrent/main/Images/6.jpg',
+        imageAdjustments: { objectPosition: "-33px 1px", width: '309px' },
+        description: '',
+        pageLink: ''
+    }
+]
+
+export const modelingProjects = [
+    {
+        title: 'RF Logger Housing',
+        image: '',
+        imageAdjustments: { objectPosition: "-52px -3px", width: '400px' },
+        description: '',
+        pageLink: ''
+    },
+    {
+        title: 'Pulse Generator Housing',
+        image: '',
+        imageAdjustments: { objectPosition: "-33px 1px", width: '309px' },
+        description: '',
+        pageLink: ''
+    },
+    {
+        title: 'Specialized Screwdriver',
+        image: '',
+        imageAdjustments: { objectPosition: "-33px 1px", width: '309px' },
+        description: '',
+        pageLink: ''
+    },
+    {
+        title: 'House Mouse VR Game',
+        image: '',
+        imageAdjustments: { objectPosition: "-33px 1px", width: '309px' },
+        description: '',
+        pageLink: ''
+    },
+    {
+        title: 'Animatronic Dinosaur',
+        image: '',
+        imageAdjustments: { objectPosition: "-33px 1px", width: '309px' },
+        description: '',
+        pageLink: ''
+    },
+    {
+        title: 'GoPro Anchor Mount',
+        image: '',
+        imageAdjustments: { objectPosition: "-33px 1px", width: '309px' },
+        description: '',
+        pageLink: ''
+    },
+    {
+        title: 'Cat Deterrent Housing',
+        image: '',
+        imageAdjustments: { objectPosition: "-33px 1px", width: '309px' },
+        description: '',
+        pageLink: ''
+    },
+]
+
+export const videoProjects = [
+    {
+        title: 'Recognition',
+        image: '',
+        imageAdjustments: { objectPosition: "-33px 1px", width: '309px' },
+        description: '',
+        pageLink: ''
+    },
+    {
+        title: 'On TV',
+        image: '',
+        imageAdjustments: { objectPosition: "-33px 1px", width: '309px' },
+        description: '',
+        pageLink: ''
+    },
+    {
+        title: 'Belay Glasses Advert',
+        image: '',
+        imageAdjustments: { objectPosition: "-33px 1px", width: '309px' },
+        description: '',
+        pageLink: ''
+    },
+    {
+        title: 'Bread Advert',
+        image: 'https://i.ytimg.com/vi/gCD0lYrGNG0/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDWn4Y5bPfCmSma2bVMSq3tvA4Z6g',
+        imageAdjustments: { objectPosition: "-51px -8px", width: '405px' },
+        description: '',
+        pageLink: ''
+    },
+]
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,8 @@ import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
 import React, { Fragment, useEffect, useRef, useState } from "react";
 import { gsap } from "gsap";
-// import { ScrollTrigger } from "../../node_modules/gsap/ScrollTrigger";
 import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
-
+import { webDevelopmentProjects, electronicProjects, modelingProjects, videoProjects } from "@/data/project_data";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -123,202 +122,30 @@ export default function Home() {
     );
   }, []);
 
-  // useEffect(() => {
-  //   const element = ref.current;
-  //   console.log(element);
-  //   gsap.fromTo(
-  //     [
-  //       element?.querySelector("#blockVideo") as HTMLDivElement,
-  //       element?.querySelector("#blockVideoTitle") as HTMLDivElement
-  //     ],
-  //     {
-  //       opacity: 0,
-  //       y: -20,
-  //     },
-  //     {
-  //       opacity: 1,
-  //       y: 0,
-  //       scrollTrigger: {
-  //         trigger: element?.querySelector("#blockVideo"),
-  //         start: "top 90%",
-  //         end: 'top 20%',
-  //         scrub: true,
-  //         markers: true
-  //       },
-  //     }
-  //   );
-  // }, []);
-
-  const webDevelopmentProjects = [
-    {
-      title: 'Meter Wiring Guide',
-      image: 'https://raw.githubusercontent.com/tdeckard2000/wiringGuide/main/images/Example.png',
-      imageAdjustments: { objectPosition: "-5px -48px", width: '532px' },
-      description: '',
-      pageLink: ''
-    },
-    {
-      title: 'Yolo Logan!',
-      image: 'https://raw.githubusercontent.com/tdeckard2000/YoloLogan/master/images/example.png',
-      imageAdjustments: { objectPosition: "-54px 10px", width: '379px' },
-      description: '',
-      pageLink: ''
-    },
-    {
-      title: 'Ted Counter',
-      image: 'https://raw.githubusercontent.com/tdeckard2000/TedCounter/master/IMG_0729.jpg',
-      imageAdjustments: { objectPosition: "-4px -44px", width: '356px' },
-      description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi 
-      ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit 
-      in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint 
-      occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
-      pageLink: ''
-    },
-    {
-      title: 'Support Ticket Dashboard',
-      image: 'https://raw.githubusercontent.com/tdeckard2000/Support-Ticket-Dashboard/master/images/example.png',
-      imageAdjustments: { objectPosition: "-17px -3px", width: '537px' },
-      description: '',
-      pageLink: ''
-    },
-    {
-      title: 'Rate-A-Coworker 2.0',
-      image: 'https://media.istockphoto.com/id/1303510161/photo/abstract-empty-room.jpg?s=612x612&w=0&k=20&c=tfKPp0pwOyXNvuRZwWryADl9jdtEFltX2Cc-C2qFfY8=',
-      imageAdjustments: { objectPosition: "-27px 1px", width: '330px' },
-      description: '',
-      pageLink: ''
-    },
-    {
-      title: 'Fancy To-Do List',
-      image: 'https://media.istockphoto.com/id/1303510161/photo/abstract-empty-room.jpg?s=612x612&w=0&k=20&c=tfKPp0pwOyXNvuRZwWryADl9jdtEFltX2Cc-C2qFfY8=',
-      imageAdjustments: { objectPosition: "-27px 1px", width: '330px' },
-      description: '',
-      pageLink: ''
-    },
-    {
-      title: 'Simon Says Game',
-      image: 'https://media.istockphoto.com/id/1303510161/photo/abstract-empty-room.jpg?s=612x612&w=0&k=20&c=tfKPp0pwOyXNvuRZwWryADl9jdtEFltX2Cc-C2qFfY8=',
-      imageAdjustments: { objectPosition: "-27px 1px", width: '330px' },
-      description: '',
-      pageLink: ''
-    },
-    {
-      title: 'Portfolio Site',
-      image: 'https://media.istockphoto.com/id/1303510161/photo/abstract-empty-room.jpg?s=612x612&w=0&k=20&c=tfKPp0pwOyXNvuRZwWryADl9jdtEFltX2Cc-C2qFfY8=',
-      imageAdjustments: { objectPosition: "-27px 1px", width: '330px' },
-      description: "You're looking at it. Just a confirmation that I did indeed make this.",
-      pageLink: ''
-    }
-  ];
-
-  const electronicProjects = [
-    {
-      title: "Rex 'Battle Bot' R/C",
-      image: 'rex.GIF',
-      imageAdjustments: { objectPosition: "-52px -3px", width: '400px' },
-      description: '',
-      pageLink: ''
-    },
-    {
-      title: 'Knock to Unlock',
-      image: 'https://raw.githubusercontent.com/tdeckard2000/door_knock_unlock_arduino/main/images/IMG_7573.jpeg',
-      imageAdjustments: { objectPosition: "-33px 1px", width: '309px' },
-      description: '',
-      pageLink: ''
-    },
-    {
-      title: 'Cat Fan Deterrent',
-      image: 'https://raw.githubusercontent.com/tdeckard2000/cat_fan_deterrent/main/Images/6.jpg',
-      imageAdjustments: { objectPosition: "-33px 1px", width: '309px' },
-      description: '',
-      pageLink: ''
-    }
-  ]
-
-  const modelingProjects = [
-    {
-      title: 'RF Logger Housing',
-      image: '',
-      imageAdjustments: { objectPosition: "-52px -3px", width: '400px' },
-      description: '',
-      pageLink: ''
-    },
-    {
-      title: 'Pulse Generator Housing',
-      image: '',
-      imageAdjustments: { objectPosition: "-33px 1px", width: '309px' },
-      description: '',
-      pageLink: ''
-    },
-    {
-      title: 'Specialized Screwdriver',
-      image: '',
-      imageAdjustments: { objectPosition: "-33px 1px", width: '309px' },
-      description: '',
-      pageLink: ''
-    },
-    {
-      title: 'House Mouse VR Game',
-      image: '',
-      imageAdjustments: { objectPosition: "-33px 1px", width: '309px' },
-      description: '',
-      pageLink: ''
-    },
-    {
-      title: 'Animatronic Dinosaur',
-      image: '',
-      imageAdjustments: { objectPosition: "-33px 1px", width: '309px' },
-      description: '',
-      pageLink: ''
-    },
-    {
-      title: 'GoPro Anchor Mount',
-      image: '',
-      imageAdjustments: { objectPosition: "-33px 1px", width: '309px' },
-      description: '',
-      pageLink: ''
-    },
-    {
-      title: 'Cat Deterrent Housing',
-      image: '',
-      imageAdjustments: { objectPosition: "-33px 1px", width: '309px' },
-      description: '',
-      pageLink: ''
-    },
-  ]
-
-  const videoProjects = [
-    {
-      title: 'Recognition',
-      image: '',
-      imageAdjustments: { objectPosition: "-33px 1px", width: '309px' },
-      description: '',
-      pageLink: ''
-    },
-    {
-      title: 'On TV',
-      image: '',
-      imageAdjustments: { objectPosition: "-33px 1px", width: '309px' },
-      description: '',
-      pageLink: ''
-    },
-    {
-      title: 'Belay Glasses Advert',
-      image: '',
-      imageAdjustments: { objectPosition: "-33px 1px", width: '309px' },
-      description: '',
-      pageLink: ''
-    },
-    {
-      title: 'Bread Advert',
-      image: 'https://i.ytimg.com/vi/gCD0lYrGNG0/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDWn4Y5bPfCmSma2bVMSq3tvA4Z6g',
-      imageAdjustments: { objectPosition: "-51px -8px", width: '405px' },
-      description: '',
-      pageLink: ''
-    },
-  ]
+  useEffect(() => {
+    const element = ref.current;
+    console.log(element);
+    gsap.fromTo(
+      [
+        element?.querySelector("#blockResume") as HTMLDivElement,
+        element?.querySelector("#blockResumeTitle") as HTMLDivElement
+      ],
+      {
+        opacity: 0,
+        y: -20,
+      },
+      {
+        opacity: 1,
+        y: 0,
+        scrollTrigger: {
+          trigger: element?.querySelector("#blockResume"),
+          start: "top 90%",
+          end: 'top 20%',
+          scrub: true,
+        },
+      }
+    );
+  }, []);
 
   return (
     <>
@@ -361,11 +188,11 @@ export default function Home() {
         <div className={styles.body}>
           <div className={styles.navContainerDesktop}
             style={{ bottom: yPosition / 4 + "px" }}>
-              <a style={{textDecoration: 'none'}} href="#blockWebDevTitle"><span>Web</span></a>
-              <a style={{textDecoration: 'none'}} href="#blockElectronicsTitle"><span>Electronics</span></a>
-              <a style={{textDecoration: 'none'}} href="#blockModelingTitle"><span>3D Modeling</span></a>
-              <a style={{textDecoration: 'none'}} href="#blockVideoTitle"><span>Video</span></a>
-              <a style={{textDecoration: 'none'}} href="#blockResumeTitle"><span>Resume</span></a>
+            <a style={{ textDecoration: 'none' }} href="#blockWebDevTitle"><span>Web</span></a>
+            <a style={{ textDecoration: 'none' }} href="#blockElectronicsTitle"><span>Electronics</span></a>
+            <a style={{ textDecoration: 'none' }} href="#blockModelingTitle"><span>3D Modeling</span></a>
+            <a style={{ textDecoration: 'none' }} href="#blockVideoTitle"><span>Video</span></a>
+            <a style={{ textDecoration: 'none' }} href="#blockResumeTitle"><span>Resume</span></a>
           </div>
           <div className={styles.blockTitle} id="blockWebDevTitle">Web Development</div>
           <div id="blockWebDev">
@@ -434,23 +261,28 @@ export default function Home() {
               </Fragment>
             )}
           </div>
+          <div className={styles.blockDivider}></div>
+          <div className={styles.blockTitle} id="blockResumeTitle">Resume</div>
+          <div id="blockResume">
+            <div className={styles.contactSection}>
+              <h1>Trent Deckard</h1>
+              <div>
+                <h3>Contact</h3>
+                <a href="tel:+8123605563">812-360-5563</a>
+                <a href="mailto: tredeckard@gmail.com">tredeckard@gmail.com</a>
+              </div>
+              <div>
+                <h3>Social Media</h3>
+                <div className={styles.socialLinks}>
+                  <a href="http://linkedin.com/in/trentdeckard" target="_blank">LinkedIn <img src="icon_new_window.svg" className={styles.iconNewWindow} /></a>
+                  <a href="https://github.com/tdeckard2000" target="_blank">GitHub <img src="icon_new_window.svg" className={styles.iconNewWindow} /></a>
+                  <a href="http://youtube.com/interestingted" target="_blank">YouTube <img src="icon_new_window.svg" className={styles.iconNewWindow}/></a>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </main>
     </>
   );
 }
-
-/* <div className={styles.contactSection}>
-            <h1>Trent Deckard</h1>
-            <div>
-              <h3>Contact</h3>
-              <a href="tel:+8123605563">812.360.5563</a>
-              <a href="mailto: tredeckard@gmail.com">tredeckard@gmail.com</a>
-            </div>
-            <div>
-              <h3>Social Media</h3>
-              <a href="http://linkedin.com/in/trentdeckard">LinkedIn</a>
-              <a href="https://github.com/tdeckard2000">GitHub</a>
-              <a href="http://youtube.com/interestingted">YouTube</a>
-            </div>
-          </div> */
