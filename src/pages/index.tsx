4313d028e85596d519a4ba9aa96f3bce129af06d
Trent Deckard <tredeckard@gmail.com>
2023-02-18 22:53:55 -0500

    Add sticky headers & more projects

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -27,7 +27,10 @@ export default function Home() {
     const element = ref.current;
     console.log(element);
     gsap.fromTo(
-      element?.querySelector("#blockWebDev") as HTMLDivElement,
+      [
+        element?.querySelector("#blockWebDev") as HTMLDivElement,
+        element?.querySelector("#blockWebDevTitle") as HTMLDivElement
+      ],
       {
         opacity: 0,
         y: -20,
@@ -45,25 +48,126 @@ export default function Home() {
     );
   }, []);
 
+  useEffect(() => {
+    const element = ref.current;
+    console.log(element);
+    gsap.fromTo(
+      [
+        element?.querySelector("#blockElectronics") as HTMLDivElement,
+        element?.querySelector("#blockElectronicsTitle") as HTMLDivElement
+      ],
+      {
+        opacity: 0,
+        y: -20,
+      },
+      {
+        opacity: 1,
+        y: 0,
+        scrollTrigger: {
+          trigger: element?.querySelector("#blockElectronics"),
+          start: "top 90%",
+          end: 'top 20%',
+          scrub: true,
+        },
+      }
+    );
+  }, []);
+
+  useEffect(() => {
+    const element = ref.current;
+    console.log(element);
+    gsap.fromTo(
+      [
+        element?.querySelector("#blockModeling") as HTMLDivElement,
+        element?.querySelector("#blockModelingTitle") as HTMLDivElement
+      ],
+      {
+        opacity: 0,
+        y: -20,
+      },
+      {
+        opacity: 1,
+        y: 0,
+        scrollTrigger: {
+          trigger: element?.querySelector("#blockModeling"),
+          start: "top 90%",
+          end: 'top 20%',
+          scrub: true,
+        },
+      }
+    );
+  }, []);
+
+  useEffect(() => {
+    const element = ref.current;
+    console.log(element);
+    gsap.fromTo(
+      [
+        element?.querySelector("#blockVideo") as HTMLDivElement,
+        element?.querySelector("#blockVideoTitle") as HTMLDivElement
+      ],
+      {
+        opacity: 0,
+        y: -20,
+      },
+      {
+        opacity: 1,
+        y: 0,
+        scrollTrigger: {
+          trigger: element?.querySelector("#blockVideo"),
+          start: "top 90%",
+          end: 'top 20%',
+          scrub: true,
+        },
+      }
+    );
+  }, []);
+
+  // useEffect(() => {
+  //   const element = ref.current;
+  //   console.log(element);
+  //   gsap.fromTo(
+  //     [
+  //       element?.querySelector("#blockVideo") as HTMLDivElement,
+  //       element?.querySelector("#blockVideoTitle") as HTMLDivElement
+  //     ],
+  //     {
+  //       opacity: 0,
+  //       y: -20,
+  //     },
+  //     {
+  //       opacity: 1,
+  //       y: 0,
+  //       scrollTrigger: {
+  //         trigger: element?.querySelector("#blockVideo"),
+  //         start: "top 90%",
+  //         end: 'top 20%',
+  //         scrub: true,
+  //         markers: true
+  //       },
+  //     }
+  //   );
+  // }, []);
+
   const webDevelopmentProjects = [
     {
       title: 'Meter Wiring Guide',
       image: 'https://raw.githubusercontent.com/tdeckard2000/wiringGuide/main/images/Example.png',
-      imageAdjustments: {objectPosition: "-5px -48px", width: '532px'},
+      imageAdjustments: { objectPosition: "-5px -48px", width: '532px' },
       description: '',
       pageLink: ''
     },
     {
       title: 'Yolo Logan!',
       image: 'https://raw.githubusercontent.com/tdeckard2000/YoloLogan/master/images/example.png',
-      imageAdjustments: {objectPosition: "-54px 10px", width: '379px'},
+      imageAdjustments: { objectPosition: "-54px 10px", width: '379px' },
       description: '',
       pageLink: ''
     },
     {
       title: 'Ted Counter',
       image: 'https://raw.githubusercontent.com/tdeckard2000/TedCounter/master/IMG_0729.jpg',
-      imageAdjustments: {objectPosition: "-4px -44px", width: '356px'},
+      imageAdjustments: { objectPosition: "-4px -44px", width: '356px' },
       description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
       sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
       Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi 
@@ -75,35 +179,35 @@ export default function Home() {
     {
       title: 'Support Ticket Dashboard',
       image: 'https://raw.githubusercontent.com/tdeckard2000/Support-Ticket-Dashboard/master/images/example.png',
-      imageAdjustments: {objectPosition: "-17px -3px", width: '537px'},
+      imageAdjustments: { objectPosition: "-17px -3px", width: '537px' },
       description: '',
       pageLink: ''
     },
     {
       title: 'Rate-A-Coworker 2.0',
       image: 'https://media.istockphoto.com/id/1303510161/photo/abstract-empty-room.jpg?s=612x612&w=0&k=20&c=tfKPp0pwOyXNvuRZwWryADl9jdtEFltX2Cc-C2qFfY8=',
-      imageAdjustments: {objectPosition: "-27px 1px", width: '330px'},
+      imageAdjustments: { objectPosition: "-27px 1px", width: '330px' },
       description: '',
       pageLink: ''
     },
     {
       title: 'Fancy To-Do List',
       image: 'https://media.istockphoto.com/id/1303510161/photo/abstract-empty-room.jpg?s=612x612&w=0&k=20&c=tfKPp0pwOyXNvuRZwWryADl9jdtEFltX2Cc-C2qFfY8=',
-      imageAdjustments: {objectPosition: "-27px 1px", width: '330px'},
+      imageAdjustments: { objectPosition: "-27px 1px", width: '330px' },
       description: '',
       pageLink: ''
     },
     {
       title: 'Simon Says Game',
       image: 'https://media.istockphoto.com/id/1303510161/photo/abstract-empty-room.jpg?s=612x612&w=0&k=20&c=tfKPp0pwOyXNvuRZwWryADl9jdtEFltX2Cc-C2qFfY8=',
-      imageAdjustments: {objectPosition: "-27px 1px", width: '330px'},
+      imageAdjustments: { objectPosition: "-27px 1px", width: '330px' },
       description: '',
       pageLink: ''
     },
     {
       title: 'Portfolio Site',
       image: 'https://media.istockphoto.com/id/1303510161/photo/abstract-empty-room.jpg?s=612x612&w=0&k=20&c=tfKPp0pwOyXNvuRZwWryADl9jdtEFltX2Cc-C2qFfY8=',
-      imageAdjustments: {objectPosition: "-27px 1px", width: '330px'},
+      imageAdjustments: { objectPosition: "-27px 1px", width: '330px' },
       description: "You're looking at it. Just a confirmation that I did indeed make this.",
       pageLink: ''
     }
@@ -111,26 +215,102 @@ export default function Home() {
 
   const electronicProjects = [
     {
-      title: 'Rex',
+      title: "Rex 'Battle Bot' R/C",
       image: 'rex.GIF',
-      imageAdjustments: {objectPosition: "-52px -3px", width: '400px'},
+      imageAdjustments: { objectPosition: "-52px -3px", width: '400px' },
       description: '',
       pageLink: ''
     },
     {
       title: 'Knock to Unlock',
       image: 'https://raw.githubusercontent.com/tdeckard2000/door_knock_unlock_arduino/main/images/IMG_7573.jpeg',
-      imageAdjustments: {objectPosition: "-33px 1px", width: '309px'},
+      imageAdjustments: { objectPosition: "-33px 1px", width: '309px' },
       description: '',
       pageLink: ''
     },
     {
       title: 'Cat Fan Deterrent',
       image: 'https://raw.githubusercontent.com/tdeckard2000/cat_fan_deterrent/main/Images/6.jpg',
-      imageAdjustments: {objectPosition: "-33px 1px", width: '309px'},
+      imageAdjustments: { objectPosition: "-33px 1px", width: '309px' },
       description: '',
       pageLink: ''
     }
+  ]
+
+  const modelingProjects = [
+    {
+      title: 'RF Logger Housing',
+      image: '',
+      imageAdjustments: { objectPosition: "-52px -3px", width: '400px' },
+      description: '',
+      pageLink: ''
+    },
+    {
+      title: 'Pulse Generator Housing',
+      image: '',
+      imageAdjustments: { objectPosition: "-33px 1px", width: '309px' },
+      description: '',
+      pageLink: ''
+    },
+    {
+      title: 'Specialized Screwdriver',
+      image: '',
+      imageAdjustments: { objectPosition: "-33px 1px", width: '309px' },
+      description: '',
+      pageLink: ''
+    },
+    {
+      title: 'House Mouse VR Game',
+      image: '',
+      imageAdjustments: { objectPosition: "-33px 1px", width: '309px' },
+      description: '',
+      pageLink: ''
+    },
+    {
+      title: 'Animatronic Dinosaur',
+      image: '',
+      imageAdjustments: { objectPosition: "-33px 1px", width: '309px' },
+      description: '',
+      pageLink: ''
+    },
+    {
+      title: 'GoPro Anchor Mount',
+      image: '',
+      imageAdjustments: { objectPosition: "-33px 1px", width: '309px' },
+      description: '',
+      pageLink: ''
+    },
+    {
+      title: 'Cat Deterrent Housing',
+      image: '',
+      imageAdjustments: { objectPosition: "-33px 1px", width: '309px' },
+      description: '',
+      pageLink: ''
+    },
+  ]
+
+  const videoProjects = [
+    {
+      title: 'Recognition',
+      image: '',
+      imageAdjustments: { objectPosition: "-33px 1px", width: '309px' },
+      description: '',
+      pageLink: ''
+    },
+    {
+      title: 'Belay Glasses Advert',
+      image: '',
+      imageAdjustments: { objectPosition: "-33px 1px", width: '309px' },
+      description: '',
+      pageLink: ''
+    },
+    {
+      title: 'Bread Advert',
+      image: 'https://i.ytimg.com/vi/gCD0lYrGNG0/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDWn4Y5bPfCmSma2bVMSq3tvA4Z6g',
+      imageAdjustments: { objectPosition: "-51px -8px", width: '405px' },
+      description: '',
+      pageLink: ''
+    },
   ]
 
   return (
@@ -176,12 +356,12 @@ export default function Home() {
             style={{ bottom: yPosition / 4 + "px" }}>
             <span>Web</span>
             <span>Electronics</span>
-            <span>Modeling</span>
+            <span>3D Modeling</span>
             <span>Video</span>
-            <span>Contact</span>
+            <span>Resume</span>
           </div>
-          <div className={styles.blockWebDev} id="blockWebDev">
-            <div className={styles.blockTitle}>Web Development</div>
+          <div className={styles.blockTitle} id="blockWebDevTitle">Web Development</div>
+          <div id="blockWebDev">
             {webDevelopmentProjects.map((project) =>
               <Fragment key={project.title}>
                 <div className={styles.blockContainer}>
@@ -189,15 +369,16 @@ export default function Home() {
                     <img className={styles.blockImage} style={project.imageAdjustments} src={project.image} alt="" />
                   </div>
                   <div className={styles.blockRightColumn}>
-                    <div style={{fontSize: '24px', margin: '30px 0'}}>{project.title}</div>
+                    <div style={{ fontSize: '24px', margin: '30px 0' }}>{project.title}</div>
                     <p>{project.description}</p>
                   </div>
                 </div>
               </Fragment>
             )}
           </div>
-          <div className={styles.blockWebDev} id="blockElectronics">
-            <div className={styles.blockTitle}>Electronics</div>
+          <div className={styles.blockDivider}></div>
+          <div className={styles.blockTitle} id="blockElectronicsTitle">Electronics</div>
+          <div id="blockElectronics">
             {electronicProjects.map((project) =>
               <Fragment key={project.title}>
                 <div className={styles.blockContainer}>
@@ -205,7 +386,41 @@ export default function Home() {
                     <img className={styles.blockImage} style={project.imageAdjustments} src={project.image} alt="" />
                   </div>
                   <div className={styles.blockRightColumn}>
-                    <div style={{fontSize: '24px', margin: '30px 0'}}>{project.title}</div>
+                    <div style={{ fontSize: '24px', margin: '30px 0' }}>{project.title}</div>
+                    <p>{project.description}</p>
+                  </div>
+                </div>
+              </Fragment>
+            )}
+          </div>
+          <div className={styles.blockDivider}></div>
+          <div className={styles.blockTitle} id="blockModelingTitle">3D Modeling</div>
+          <div id="blockModeling">
+            {modelingProjects.map((project) =>
+              <Fragment key={project.title}>
+                <div className={styles.blockContainer}>
+                  <div className={styles.blockImageContainer}>
+                    <img className={styles.blockImage} style={project.imageAdjustments} src={project.image} alt="" />
+                  </div>
+                  <div className={styles.blockRightColumn}>
+                    <div style={{ fontSize: '24px', margin: '30px 0' }}>{project.title}</div>
+                    <p>{project.description}</p>
+                  </div>
+                </div>
+              </Fragment>
+            )}
+          </div>
+          <div className={styles.blockDivider}></div>
+          <div className={styles.blockTitle} id="blockVideoTitle">Video</div>
+          <div id="blockVideo">
+            {videoProjects.map((project) =>
+              <Fragment key={project.title}>
+                <div className={styles.blockContainer}>
+                  <div className={styles.blockImageContainer}>
+                    <img className={styles.blockImage} style={project.imageAdjustments} src={project.image} alt="" />
+                  </div>
+                  <div className={styles.blockRightColumn}>
+                    <div style={{ fontSize: '24px', margin: '30px 0' }}>{project.title}</div>
                     <p>{project.description}</p>
                   </div>
                 </div>
